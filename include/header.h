be5a52cf471d40c73802f4cd00bb3f3d5c9c07d8
LoickBde <loick.baude@ig2i.centralelille.fr>
2020-01-09 09:04:10 +0100

    Add request description

--- include/header.h
+++ include/header.h
@@ -9,4 +9,13 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <sys/select.h>
+#define CONNECT_SRV     "100:Demande de connection au serveur"
+#define CREATE_LOB      "200:Crée un salon de jeu"
+#define CONNECT_LOB     "300:Demande de connexion à un salon de jeu"    
+#define ATTACK          "400:Attaque le plateau ennemie à une position donnée"
+#define CONNECT_SRV_OK  "102:Connexion au serveur acceptée"
+#define CONNECT_LOB_OK  "302:Connexion à une salle acceptée"
+#define RESULT_ATK      "500:Résultat d'une attaque d'un joueur"
+#define ERREUR          "KO:Erreur survenue"
+
 #define NB_PLAYER 2
