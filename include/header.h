dab0ddacb5f606bdc41a643f3cf1768093cd9df0
Nathan <capiauxnathan@gmail.com>
2020-01-12 19:27:05 +0100

    add request code + macro

--- include/header.h
+++ include/header.h
@@ -1,4 +1,3 @@
-#define CHECK(sts,msg) if ((sts) == -1) {perror(msg);exit(-1);}
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,6 +8,10 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <sys/select.h>
+
+#define CHECK(sts,msg) if ((sts) == -1) {perror(msg);exit(-1);}
+#define NB_ELEM(tab) sizeof((tab))/sizeof((tab[0]))
+
 #define NB_PLAYER 20
 #define MAX_BUFF 1024
 #define MAX_LENGTH_IP 15
@@ -18,10 +21,12 @@ char buffer[MAX_BUFF];
 #define CONNECT_SRV     100
 #define CREATE_LOB      200
 #define CONNECT_LOB     300
+#define PRINT_LOB       150
 
 #define ATTACK          400
 #define CONNECT_SRV_OK  102
 #define CONNECT_LOB_OK  302
 #define RESULT_ATK      500
+#define DISCONNECT      600
 #define ERREUR          "KO:Une erreur est survenue ! Réesayer"
 
